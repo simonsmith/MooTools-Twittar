c3ac5a643e89bb9885ef6e17891c7f5492ce3178
simonsmith <ssmith@qmetric.co.uk>
2012-03-27 16:30:32 +0100

    Code formatting

--- src/Twittar.js
+++ src/Twittar.js
@@ -1,150 +1,147 @@
-	
-	/*
-	---
-	
-	description: Twittar Class. Request a users' recent tweets and displays them
 
-	author:
-	- Simon Smith - http://blink-design.net
+/*
+---
 
-	requires:
-	- core/1.4.1:*
-	- more:1.4.0.1:Request.JSONP
+description: Twittar Class. Request a users' recent tweets and displays them
 
-	provides: [Twittar]
+author:
+- Simon Smith - http://blink-design.net
 
-	...
-	*/
+requires:
+- core/1.4.1:*
+- more:1.4.0.1:Request.JSONP
 
-	var Twittar = new Class({
-		
-		Implements : [ Options, Events ],
-		
-		initialize : function( username, element, options ) {
+provides: [Twittar]
 
-			this.setOptions( options );
+...
+*/
 
-			this.username = username;
-			this.element = $( element );
-			this.url = this.formatUrl();
-			
-			this.request = new Request.JSONP({
-				url : this.url,
-				method : 'get',
-				timeout : 5000,
-				onRequest : function() {
-					this.fireEvent('request', [ this.element ]);
-				}.bind( this ),
-				onTimeout : function() {
-					this.fireEvent('timeout', [ this.element ]);
-				}.bind( this ),
-				onComplete : this.buildTweets.bind( this )
-			});
+var Twittar = new Class({
 
-		},
-		 
-		options : {
-			twitterUrl : 'https://api.twitter.com/1/statuses/user_timeline.json?&exclude_replies={replies}&contributor_details={contributerDetails}&include_rts={retweets}&screen_name={username}&count={count}',
-			showRetweets : true,
-			showReplies : true,
-			contributerDetails : false,
-			tweetCount : 5,
-			showUserInfo : true,
-			userInfoElement : 'a',
-			onComplete : function( element, container ) {
-			
-				element.grab( container );
+    Implements: [Options, Events],
 
-			}
-		},
-		
-		formatUrl : function() {
+    initialize: function(username, element, options) {
 
-			var options = this.options;
+        this.setOptions(options);
 
-			return options.twitterUrl.substitute({
-				retweets : options.showRetweets,
-				replies : options.showReplies,
-				count : options.tweetCount,
-				contributerDetails : options.contributerDetails,
-				username : this.username
-			});
+        this.username = username;
+        this.element = $(element);
+        this.url = this.formatUrl();
 
-		},
-		
-		linkify : function(text) {
+        this.request = new Request.JSONP({
+            url: this.url,
+            method: 'get',
+            timeout: 5000,
+            onRequest: function() {
+                this.fireEvent('request', [this.element]);
+            }.bind(this),
+            onTimeout: function() {
+                this.fireEvent('timeout', [this.element]);
+            }.bind(this),
+            onComplete: this.buildTweets.bind(this)
+        });
 
-			// Pinched from - http://goo.gl/HMk15
-			return text.replace(/(https?:\/\/\S+)/gi,'<a href="$1">$1</a>')
-				   .replace(/(^|\s)@(\w+)/g,'$1<a href="http://twitter.com/$2">@$2</a>')
-				   .replace(/(^|\s)#(\w+)/g,'$1#<a href="http://search.twitter.com/search?q=%23$2">$2</a>');
+    },
 
-		},
-		
-		getTweets : function() {
+    options: {
+        twitterUrl: 'https://api.twitter.com/1/statuses/user_timeline.json?&exclude_replies={replies}&contributor_details={contributerDetails}&include_rts={retweets}&screen_name={username}&count={count}',
+        showRetweets: true,
+        showReplies: true,
+        contributerDetails: false,
+        tweetCount: 5,
+        showUserInfo: true,
+        userInfoElement: 'a',
+        onComplete: function(element, container) {
+            element.grab(container);
+        }
+    },
 
-			this.request.send();				
+    formatUrl: function() {
 
-			return this;
+        var options = this.options;
 
-		},
-		
-		formatTweetDate : function( dateStr ) {
+        return options.twitterUrl.substitute({
+            retweets: options.showRetweets,
+            replies: options.showReplies,
+            count: options.tweetCount,
+            contributerDetails: options.contributerDetails,
+            username: this.username
+        });
 
-			// http://goo.gl/p3qd4
-			dateStr = dateStr.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/, '$1 $2 $4 $3 UTC');
-			return new Date( dateStr ).toUTCString();
+    },
 
-		},
-		
-		formatTweetLink : function( user, id ) {
+    linkify: function(text) {
 
-			return 'http://twitter.com/{user}/status/{id}'.substitute({
-				user : user,
-				id : id
-			});
+        // Pinched from - http://goo.gl/HMk15
+        return text.replace(/(https?:\/\/\S+)/gi,'<a href="$1">$1</a>')
+               .replace(/(^|\s)@(\w+)/g,'$1<a href="http://twitter.com/$2">@$2</a>')
+               .replace(/(^|\s)#(\w+)/g,'$1#<a href="http://search.twitter.com/search?q=%23$2">$2</a>');
 
-		},
-		
-		buildTweets : function( tweets ) {
+    },
 
-			if ( tweets.length == 0 ) return;
+    getTweets: function() {
 
-			var list = new Element('ul'),
-				container = new Element('div', { 'class' : 'tweets' });
-				
-			if ( this.options.showUserInfo ) {
-				
-				container.grab( new Element(this.options.userInfoElement, {
-					'class' : 'twitter-user',
-					text : '@' + tweets[0].user.screen_name,
-					href : 'http://twitter.com/' + tweets[0].user.screen_name
-				}), 'top');
-				
-			}	
+        this.request.send();
+        return this;
 
-			Object.each( tweets, function( item ) {
+    },
 
-				var element = new Element('li', {
-					'class' : 'tweet'
-				}),
-				tweet = new Element('span', {
-					'class' : 'tweet-text',
-					html : this.linkify( item.text )
-				}),
-				date = new Element('a', {
-					'class' : 'tweet-date',
-					text : this.formatTweetDate( item.created_at ),
-					href : this.formatTweetLink( item.user.screen_name, item.id_str )
-				});
-				
-				element.grab( tweet ).grab( date ).inject( list );
-				list.inject( container );
+    formatTweetDate: function(dateStr) {
 
-			}, this);
+        // http://goo.gl/p3qd4
+        dateStr = dateStr.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/, '$1 $2 $4 $3 UTC');
+        return new Date(dateStr).toUTCString();
 
-			this.fireEvent('complete', [ this.element, container, list, list.getChildren(), tweets ]);
+    },
 
-		}
-		
-	});
+    formatTweetLink : function(user, id) {
+
+        return 'http://twitter.com/{user}/status/{id}'.substitute({
+            user: user,
+            id: id
+        });
+
+    },
+
+    buildTweets: function(tweets) {
+
+        if (tweets.length == 0) return;
+
+        var list = new Element('ul'),
+            container = new Element('div', { 'class': 'tweets' });
+
+        if (this.options.showUserInfo) {
+
+            container.grab(new Element(this.options.userInfoElement, {
+                'class': 'twitter-user',
+                text: '@' + tweets[0].user.screen_name,
+                href: 'http://twitter.com/' + tweets[0].user.screen_name
+            }), 'top');
+
+        }
+
+        Object.each(tweets, function(item) {
+
+            var element = new Element('li', {
+                'class': 'tweet'
+            }),
+            tweet = new Element('span', {
+                'class': 'tweet-text',
+                html: this.linkify(item.text)
+            }),
+            date = new Element('a', {
+                'class': 'tweet-date',
+                text: this.formatTweetDate(item.created_at),
+                href: this.formatTweetLink(item.user.screen_name, item.id_str)
+            });
+
+            element.grab(tweet).grab(date).inject(list);
+            list.inject(container);
+
+        }, this);
+
+        this.fireEvent('complete', [this.element, container, list, list.getChildren(), tweets]);
+
+    }
+
+});
